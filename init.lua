a3eebc4036d93c1574fcd1846f5e6b8797a2dd2f
riku929hr <33782267+riku929hr@users.noreply.github.com>
2023-07-03 22:17:38 +0900

    set lsp timeout 10000ms

--- init.lua
+++ init.lua
@@ -39,11 +39,11 @@ return {
           -- "python",
         },
       },
-      disabled = {       -- disable formatting capabilities for the listed language servers
+      disabled = {        -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-        "intelephense",  -- use php-cs-fixer to format codes
+        "intelephense",   -- use php-cs-fixer to format codes
       },
-      timeout_ms = 1000, -- default format timeout
+      timeout_ms = 10000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end
--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -8,9 +8,10 @@ return {
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
-      -- Set a formatter
-      null_ls.builtins.formatting.prettier,
+      -- php
       null_ls.builtins.formatting.phpcsfixer,
+      -- js/ts
+      null_ls.builtins.formatting.prettier,
       null_ls.builtins.code_actions.eslint_d,
     }
     return config -- return final config table
