b47a53862986d13f5c375e0adbec45be68608f01
riku929hr <33782267+riku929hr@users.noreply.github.com>
2023-07-15 22:21:34 +0900

    remove no need treesitter setting

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,21 +3,9 @@ return {
   opts = function(_, opts)
     opts.ensure_installed = "all"
     -- opts.context_commentstring = true -- already loaded in astronvim/comment.nvim
-    opts.textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ['if'] = '@function.inner',
-          ['af'] = '@function.outer',
-          ['ia'] = '@parameter.inner',
-          ['aa'] = '@parameter.outer',
-        }
-      }
-    }
   end,
   dependencies = {
     -- 'JoosepAlviste/nvim-ts-context-commentstring', -- this has been already loaded in astronvim/comment.nvim
-    'nvim-treesitter/nvim-treesitter-textobjects',
+    -- 'nvim-treesitter/nvim-treesitter-textobjects', -- this has been already loaded in astronvim/comment.nvim
   }
 }
