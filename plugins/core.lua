7a48d331ac41f22eed5d10a661c3f4cdc3b164ba
riku929hr <33782267+riku929hr@users.noreply.github.com>
2023-09-07 22:51:34 +0900

    add github copilot

--- plugins/core.lua
+++ plugins/core.lua
@@ -10,6 +10,22 @@ return {
       return opts
     end,
   },
+  { import = "astrocommunity.completion.copilot-lua" },
+  { -- further customize the options set by the community
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
+  },
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
   --
